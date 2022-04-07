c8893732ef62f1363f9625c711e196a449f45860
yulio64neos <julio64neos@gmail.com>
2022-04-06 22:22:41 -0500

    Se agregó nuevas vistas

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: "App",
+
+    data:() => ({
+        drawer: false,
+        tab: null,
+        items: [
+            {
+                name: "Home",
+                url: "/",
+                btn: true,
+                icon: "fas fa*home"
+            },
+            {
+                name: "Meta",
+                url: "/meta",
+                btn: true,
+                icon: "fas fa*home"
+            },
+            {
+                name: "Company",
+                url: "/company",
+                btn: true,
+                icon: "fas fa*home"
+            },
+            {
+                name: "Support",
+                url: "/support",
+                btn: true,
+                icon: "fas fa*home"
+            },
+            {
+                name: "Contact",
+                url: "/contact",
+                btn: true,
+                icon: "fas fa*home"
+            },
+        ]
+    })
+}
+</script>
+
+<style>
+
+</style>
